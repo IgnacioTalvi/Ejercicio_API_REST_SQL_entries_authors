c8c78f6a65814696bea55ed31953669393b67623
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-04-23 13:51:22 +0200

    add/app.js setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express"); // importar express
+const app = express(); // crear el servidor --> Objeto de la clase express
+const port = 3000; // puerto donde se ejecuta el servidor
+
+app.use(express.json()); // middleware para parsear el body a JSON
+
+// GET http://localhost:3000/
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Mi app va a estar escuchand en el puerto 3000
+// Lanzar el servidor
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
