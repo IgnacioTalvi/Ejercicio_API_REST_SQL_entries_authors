f351eb227033cb184f1f0df2c303ebad43c0f482
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-05-04 15:32:30 +0200

    add/delete-author-querie

--- queries/author.queries.js
+++ queries/author.queries.js
@@ -11,16 +11,25 @@ const createAuthor = `
     VALUES ($1, $2, $3, $4)
     `;
 
-const queries = `SELECT 
-      entries.title, 
-      entries.content, 
-      entries.date, 
-      entries.category,
-      authors.name, 
-      authors.surname, 
-      authors.image
-    FROM entries
-    LEFT JOIN authors ON entries.id_author = authors.id_author
-    ORDER BY entries.title ASC`;
+const deleteAuthor = `DELETE FROM authors
+    WHERE email = $1
+    RETURNING *`;
 
-module.exports = { getAllAuthors, getAuthorByEmail, createAuthor };
+// const queries = `SELECT
+//       entries.title,
+//       entries.content,
+//       entries.date,
+//       entries.category,
+//       authors.name,
+//       authors.surname,
+//       authors.image
+//     FROM entries
+//     LEFT JOIN authors ON entries.id_author = authors.id_author
+//     ORDER BY entries.title ASC`;
+
+module.exports = {
+  getAllAuthors,
+  getAuthorByEmail,
+  createAuthor,
+  deleteAuthor,
+};
